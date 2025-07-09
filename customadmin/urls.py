0002bbcc85d781306e34a654e10c5eb4e511283e
Gairolarishav <101463798+Gairolarishav@users.noreply.github.com>
2025-07-09 18:18:45 +0530

    Update urls.py

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.shortcuts import redirect
-from retells.admin import custom_admin_site
+from FastPromos.admin import custom_admin_site
 from FastPromos.views import chatbot
 
 urlpatterns = [
