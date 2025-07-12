a59fd3e2297671232dd4fa66a847d419424b5a51
Gairolarishav <101463798+Gairolarishav@users.noreply.github.com>
2025-07-12 19:50:01 +0530

    Update urls.py

--- FastPromos/urls.py
+++ FastPromos/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('add-faqs/', add_faqs, name='add_faqs'),
     path('upload-faqs/', upload_faqs_to_voiceflow, name='upload_faqs_to_voiceflow'),
     path('quote-request/', quote_request, name='quote_request'),
+    path('generate-quote/', generate_quote, name='generate_quote'),
     path('', include(router.urls)),
     path('transcript/', get_transcript, name='transcript'),
     path('chat/', chatbot, name='chatbot')
